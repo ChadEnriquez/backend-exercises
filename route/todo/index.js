e406ad5d2c0a85e2ca6b4329e14891a3f2fb6723
Chad Enriquez <73896791+ChadEnriquez@users.noreply.github.com>
2021-01-17 05:38:16 +0800

    feat: add create todo

---
+++ route/todo/index.js
@@ -0,0 +1,13 @@
+const { create } = require("./create")
+
+/**
+ * initialize routes for creating todo
+ * @param {*} app 
+ */
+exports.todo = (app) => {
+    create(app)
+}
+
+/**
+ * ENRIQUEZ, CHAD ANDREI A.
+ */
