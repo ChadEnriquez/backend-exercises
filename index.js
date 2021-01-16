e16d5a9481f9ffd2edbf368a9bfd69dd66447994
Chad Enriquez <73896791+ChadEnriquez@users.noreply.github.com>
2021-01-16 17:30:43 +0800

    feat: adds working server

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello World")
+const Fastify = require("fastify");
+
+//initialize the server = Fastify
+const server = Fastify({
+    logger: true,
+    trustProxy: true,  
+});
+
+//block to access root address
+server.get("/", {
+    //object
+    /**
+     * @param {*} req //request parameter which is sent by client 
+     */
+    handler: async (req) => {
+        console.log("Hello World!");
+        //response in JSON format
+        return { success: true } 
+    }
+
+})
+async function start () {
+    //get port from env variable
+    //IF export PORT=8000 && node index.js
+    //then port = 8000 else default is 8080
+    const port = parseInt(process.env.PORT || "8080")
+    const address = "0.0.0.0";
+    const addr = await server.listen(port, address);
+    console.log(`Listening on ${addr}`); 
+}
+
+start()
