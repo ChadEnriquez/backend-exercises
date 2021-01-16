5f4f1199b8c305e8a2786131fccbeef63fb23139
Chad Enriquez <73896791+ChadEnriquez@users.noreply.github.com>
2021-01-17 05:36:31 +0800

    refactor: put it on routes

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const { build } = require('./app');
+const { build } = require('./app')
 
 /**
  * starts app
  */
 async function start () {
     //call build app 
-    const app = await build();
+    const app = await build()
     //get port from env variable
     //IF export PORT=8000 && node index.js
     //then port = 8000 else default is 8080
     const port = parseInt(process.env.PORT || "8080")
-    const address = "0.0.0.0";
-    const addr = await app.listen(port, address);
-    console.log(`Listening on ${addr}`); 
+    const address = "0.0.0.0"
+    const addr = await app.listen(port, address)
+    console.log(`Listening on ${addr}`) 
 }
 
 start()
+
+/**
+ * ENRIQUEZ, CHAD ANDREI A.
+ */
