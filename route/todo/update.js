c97423537065f939209a71d5c5f982b8f8d3f731
Ian Salazar <iisalazar@up.edu.ph>
2021-01-18 15:49:45 +0800

    fix: working update feature "finally"

--- route/todo/update.js
+++ route/todo/update.js
@@ -1,67 +1,63 @@
-const { getTodos } = require("../../lib/get-todos")
-const { writeFileSync } = require("fs")
-const { join } = require("path")
+const { getTodos } = require("../../lib/get-todos");
+const { writeFileSync } = require("fs");
+const { join } = require("path");
 /**
  * Update one todos
- * 
- * @param {*} app 
+ *
+ * @param {*} app
  */
-exports.update = app => {
-    /**
-     * Update one todo from database
-     * give unique ID and payload
-     * @param {import('fastify').FastifyRequest} request
-     * @param {import('fastify').FastifyReply<Response>} response 
-     */
-    
-    // "/todo/:id"
-    app.put("/todo/:id", (request, response) => {
-        const { params, body } = request
-        const { id } = params
-        //gets text and done
-        const { text, done } = body || {}
-        const filename = join(__dirname, "../../database.json")
-        const encoding = "utf8"
-        const todos = getTodos(filename, encoding)
+exports.update = (app) => {
+	/**
+	 * Update one todo from database
+	 * give unique ID and payload
+	 * @param {import('fastify').FastifyRequest} request
+	 * @param {import('fastify').FastifyReply<Response>} response
+	 */
 
-        const index = todos.findIndex(todo => todo.id === id)
+	// "/todo/:id"
+	app.put("/todo/:id", (request, response) => {
+		const { params, body } = request;
+		const { id } = params;
+		//gets text and done
+		const { text, done } = body || {};
+		const filename = join(__dirname, "../../database.json");
+		const encoding = "utf8";
+		const todos = getTodos(filename, encoding);
 
-        if (index < 0) {
-            return response
-                .code(404)
-                .send({
-                    success: false, 
-                    code: "todo/not-found",
-                    message: "Todo doesnt exist"
-                })
-        }
-        //expects at least a text or done property
-        if (!text && (done === null || done === undefined)) {
-            return response
-                .code(400)
-                .send({
-                    success: false, 
-                    code: "todo/malformed",
-                    message: "Payload doesnt have text property"
-                })
-        }
-        const data = todos[index]
+		const index = todos.findIndex((todo) => todo.id === id);
 
-        if (text) {
-            data.text = text
-        }
-        if (done) {
-            data.done = done
-        }
+		if (index < 0) {
+			return response.code(404).send({
+				success: false,
+				code: "todo/not-found",
+				message: "Todo doesnt exist",
+			});
+		}
+		//expects at least a text or done property
+		if (!text && (done === null || done === undefined)) {
+			return response.code(400).send({
+				success: false,
+				code: "todo/malformed",
+				message: "Payload doesnt have text property",
+			});
+		}
+		const data = todos[index];
 
-        todos[index] = data
+		if (text) {
+			data.text = text;
+		}
+		if (done) {
+			data.done = done;
+		}
 
-        const newDatabaseStringConts = JSON.stringify({todos}, null, 2)
-        writeFileSync(filename, newDatabaseStringConts, encoding)
-        
-        return {
-            success: true,
-            data
-        }
-    })
-}
+		todos[index] = data;
+
+		const newDatabaseStringConts = JSON.stringify({ todos }, null, 2);
+		writeFileSync(filename, newDatabaseStringConts, encoding);
+
+		return {
+			success: true,
+			data,
+		};
+	});
+};
